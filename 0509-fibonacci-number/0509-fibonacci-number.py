a357ab2e4606a417f4a186495f74f33dce894a6f
rajivjha0599 <64460459+rajivjha0599@users.noreply.github.com>
2024-01-08 23:53:03 +0530

    Time: 38 ms (70.34%), Space: 17.5 MB (9.03%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,10 @@
+from math import sqrt as s
 class Solution:
     def fib(self, n: int) -> int:
-        if(n==0 or n==1):
-            return n
-        else:
-            return self.fib(n-1)+self.fib(n-2)
+        # if(n==0 or n==1):
+        #     return n
+        # else:
+        #     return self.fib(n-1)+self.fib(n-2)
+        phi= (s(5)+1)/2
+        return int(((phi**n)-((-phi)**(-1*n)))/s(5))
         
