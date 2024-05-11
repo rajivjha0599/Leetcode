87636c3c7cdec3dd22a4316a5a0d1c9775018203
rajivjha0599 <64460459+rajivjha0599@users.noreply.github.com>
2024-05-11 13:11:25 +0530

    Time: 37 ms (96.07%), Space: 16.9 MB (91.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,14 +2,21 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # return "".join(sorted(s)) == "".join(sorted(t))
        
-        if len(s) != len(t):
-            return False
-        counts,countt ={},{}
-        for i in range(len(s)):
-            counts[s[i]] = 1 + counts.get(s[i],0)
-            countt[t[i]] = 1 + countt.get(t[i],0)
-        for ch in s:
-            if counts[ch] != countt.get(ch,0):
-                return False
-        return True
-        
+#         if len(s) != len(t):
+#             return False
+#         counts,countt ={},{}
+#         for i in range(len(s)):
+#             counts[s[i]] = 1 + counts.get(s[i],0)
+#             countt[t[i]] = 1 + countt.get(t[i],0)
+#         for ch in s:
+#             if counts[ch] != countt.get(ch,0):
+#                 return False
+#         return True
+       
+            if len(s) != len(t):
+              return False
+
+            count = collections.Counter(s)
+            count.subtract(collections.Counter(t))
+            return all(freq == 0 for freq in count.values())
+          
