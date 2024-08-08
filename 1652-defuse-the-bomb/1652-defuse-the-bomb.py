8c8a4953676ea5faf55c15db5f4cd1a71e8b31b2
rajivjha0599 <64460459+rajivjha0599@users.noreply.github.com>
2024-08-09 00:27:53 +0530

    Time: 42 ms (63.75%), Space: 16.6 MB (53.66%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -1,23 +1,21 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        if(k==0):
-                return [0]*len(code)
-        temparr = list(code)
-        if(k>0):
+        n = len(code)
+        ans = n*[0] 
+        if k ==0:
+            return ans
+        start = 1 if k>0 else n+k
+        end = k if k>0 else n-1
+        
+        sum = 0
+        for i in range(start,end+1):
+            sum += code[i]
+        
+        for i in range(n):
+            ans[i] = sum
+            sum -= code[start%n]
+            start +=1
+            end +=1
+            sum += code[end%n]
+        return ans
             
-            for i in range(len(code)):
-                temp = k
-                code[i] = 0
-                while temp > 0:
-                    code[i] += temparr[(i+temp)%len(temparr)]
-                    temp-=1
-            return code
-        if(k<0):
-
-            for i in range(len(code)):
-                temp = k
-                code[i] = 0
-                while temp < 0:
-                    code[i] += temparr[(i+temp)%len(code)]
-                    temp+=1
-            return code
