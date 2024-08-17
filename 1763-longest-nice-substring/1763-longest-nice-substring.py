e10e794ade47c62cbd4fcf7e36b32147134e7e9f
rajivjha0599 <64460459+rajivjha0599@users.noreply.github.com>
2024-08-17 19:15:49 +0530

    Time: 42 ms (64.36%), Space: 16.5 MB (45.94%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -1,22 +1,13 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
-        longest = ""
-        def is_nice(s:str):
-            data = set(s)
-            for c in data:
-                if c.islower():
-                    if c.upper() not in data:
-                        return False
-                else:
-                    if c.lower() not in data:
-                        return False
-            return True
-        for i in range(len(s)):
-            n = len(s)
-            for j in range(i+1,n+1):
-                subs = s[i:j]
-                
-                if is_nice(subs) and len(subs)>len(longest):
-                    longest = subs
-        return longest
+        if len(s)<2:
+            return ""
+        data = set(s)
+        for i,c in enumerate(s):
+            if c.swapcase() not in data:
+                prefix = self.longestNiceSubstring(s[:i])
+                suffix = self.longestNiceSubstring(s[i+1:])
+                return prefix if len(prefix)>=len(suffix) else suffix
+        return s
+            
                         
