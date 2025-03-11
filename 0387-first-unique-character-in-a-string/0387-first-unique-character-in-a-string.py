c4f9361e4a59f9ed3c25d2065687800a1907b8dd
rajivjha0599 <64460459+rajivjha0599@users.noreply.github.com>
2025-03-11 09:31:17 +0530

    Time: 96 ms (10.24%), Space: 18 MB (55.16%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -5,11 +5,6 @@ class Solution:
             if s[i] not in HM:
                 HM[s[i]] = 0
             HM[s[i]] += 1
-        ans  = ""
-        for key,val in HM.items():
-            if(val == 1):
-                ans = key
-                break
-        for i in range(len(s)):
-            if s[i] == ans:return i
+        for i,c in enumerate(s):
+            if HM[c] == 1:return i
         return -1
